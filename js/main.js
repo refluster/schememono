00231596ed8eccb9b00bdb91ca09a30092f96ae5
refluster <refluster@gmail.com>
2015-08-06 08:17:30 +0900

    implement animation on hiding navigation menu

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 var Menu = function() {
 	this.menuButton = $("#menu-button");
 	this.navi = $("#navi");
-
+	this.naviBg = $("#navi-bg");
+	
 	this.hidden();
 	this.menuButton.click(this.show.bind(this));
+	this.naviBg.click(this.hidden.bind(this));
 };
 Menu.prototype.show = function() {
-	console.log("click");
 	this.navi.removeClass("navi-hidden");
+	this.naviBg.css('display', 'block');
 };
 Menu.prototype.hidden = function() {
 	this.navi.addClass("navi-hidden");
+	this.naviBg.css('display', 'none');
 };
 
 $(function() {
 	var menu = new Menu();
 });
-
