60ab4220292251c25af9da1bdb342f5cb2775ff1
refluster <refluster@gmail.com>
2015-08-06 08:00:20 +0900

    implement animation for appearing navigation menu

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@ var Menu = function() {
 	this.menuButton = $("#menu-button");
 	this.navi = $("#navi");
 
-	this.navi.addClass("navi-hidden");
+	this.hidden();
 	this.menuButton.click(this.show.bind(this));
-	console.log(this.navi);
 };
 Menu.prototype.show = function() {
 	console.log("click");
 	this.navi.removeClass("navi-hidden");
 };
 Menu.prototype.hidden = function() {
+	this.navi.addClass("navi-hidden");
 };
 
 $(function() {
